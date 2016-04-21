d6f984dbf5dd20ac14905fbbf97cc69680f69f0d
Navin Sasikumar <nsasikumar@connectify.me>
2016-04-21 12:57:26 -0400

    Add else statement

--- runit/fluentd.sh
+++ runit/fluentd.sh
@@ -2,4 +2,6 @@
 
 if [[ -n "$KINESIS_STREAM" && -n "$AWS_ACCESS_KEY_ID" && -n "$AWS_SECRET_ACCESS_KEY" && -n "$KINESIS_REGION" ]]; then
   exec /usr/local/bin/fluentd --use-v1-config -c /etc/fluent/fluent.conf -vv >>/var/log/fluentd.log 2>&1
+else
+  echo Not running fluentd.
 fi
